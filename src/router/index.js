c54c55747d91f089278d98a095a8593248e06529
kodevasse <kodevasse@outlook.com>
2021-10-10 12:22:15 +0200

    Vue 3 shopping bag with vue-router

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,29 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import { createRouter, createWebHashHistory } from 'vue-router';
+import Home from '../views/Home.vue';
+import Basket from '../views/Basket.vue';
+import Product from '../views/Product.vue';
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    path: '/basket',
+    name: 'Basket',
+    component: Basket,
+  },
+  {
+    path: '/product',
+    name: 'Product',
+    component: Product,
+  },
+];
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
---
+++ src/views/Product.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+ This is the product page
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Product',
+
+  methods: {
+   
+  },
+ 
+}
+</script>
+
+<style lang="scss">
+
+</style>
